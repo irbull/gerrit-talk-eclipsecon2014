753dd7d3eaf28b1b2171b8ab6c19eb9e14646f08
Ian Bull <irbull@eclipsesource.com>
2014-03-12 21:25:06 -0700

    Created the sample tutorial gerrit plugin
    This is the shell for the example and a single ssh module
    that prints the uptime. The required library (PrettyPrint)
    is in the libs/ folder. This needs to be installed in
    gerrit for this plugin to work.

---
+++ gerrit-plugin-tutorial/src/main/java/com/googlesource/gerrit/plugins/tutorialplugin/PrintUptime.java
@@ -0,0 +1,23 @@
+package com.googlesource.gerrit.plugins.tutorialplugin;
+
+import com.google.gerrit.sshd.CommandMetaData;
+import com.google.gerrit.sshd.SshCommand;
+import com.google.inject.Inject;
+
+import org.ocpsoft.prettytime.PrettyTime;
+
+import java.lang.management.ManagementFactory;
+import java.util.Date;
+
+@CommandMetaData(name = "uptime", description = "Print server uptime")
+public class PrintUptime extends SshCommand {
+  @Inject
+  PrintUptime() {
+  }
+  @Override
+  protected void run() {
+      Date startTime = new Date(ManagementFactory.getRuntimeMXBean().getStartTime());
+      String prettyPrintedTime =  new PrettyTime(new Date()).format(startTime);
+      stdout.println("The Gerrit server started about " + prettyPrintedTime );
+  }
+}
