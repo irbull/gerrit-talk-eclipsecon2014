33862a46ad57f820d382f5a734e43a3972eed433
Ian Bull <irbull@eclipsesource.com>
2014-03-16 23:43:31 -0700

    Updated the EclipseCon menu to include a link to create work item

--- gerrit-plugin-tutorial/src/main/java/com/googlesource/gerrit/plugins/tutorialplugin/EclipseConTopLevelMenu.java
+++ gerrit-plugin-tutorial/src/main/java/com/googlesource/gerrit/plugins/tutorialplugin/EclipseConTopLevelMenu.java
@@ -11,7 +11,8 @@ public class EclipseConTopLevelMenu implements TopMenu {
   public List<MenuEntry> getEntries() {
     return Lists.newArrayList(
         new MenuEntry("EclipseCon", Lists.newArrayList(
-               new MenuItem("EclipseCon 2014", "http://www.eclipsecon.org/na2014/"))));
+               new MenuItem("EclipseCon 2014", "http://www.eclipsecon.org/na2014/"),
+               new MenuItem("Create work item", "/#/x/tutorialPlugin/"))));
   }
 
 }
